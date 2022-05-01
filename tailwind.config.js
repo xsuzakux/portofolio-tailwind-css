22842c89c603c368569c49a0e8b77dba6e51f7fb
xsuzakux <hendrik110693@gmail.com>
2022-05-01 12:15:38 +0700

    Portofolio hendrik lega kurniawan tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container : {
+      center :true,
+      padding :'16px'
+    },
+    extend: {
+      colors : {
+        primary : '#14B8A6',
+        secondary :'#64748B',
+        dark : '#0F172A'
+      },
+      screens :{
+        '2xl' :'1320px',
+      },
+    },
+  },
+  plugins: [],
+}
